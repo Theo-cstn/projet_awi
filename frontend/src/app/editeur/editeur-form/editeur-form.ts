78d8023e6c84b3e9f72a310582bfcd52ebfcc6bd
theo <theo.constantin01@etu.umontpellier.fr>
2025-11-13 09:36:05 +0100

    1ere impl editeur-form

--- frontend/src/app/editeur/editeur-form/editeur-form.ts
+++ frontend/src/app/editeur/editeur-form/editeur-form.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-editeur-form',
@@ -7,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrl: './editeur-form.css',
 })
 export class EditeurForm {
+  readonly form = new FormGroup({  
+    
+      name: new FormControl('', {
+      nonNullable : true,
+      validators: [Validators.required, Validators.minLength(3)]
+    }),
+    
+  });
 
+  add = output<any>()
+
+  submit():void {
+    console.log(this.form.value);
+  }
+
+  getErrorMessage(control:AbstractControl|null): string|null {
+    if (control != null){  
+      if (control.errors?.['required']) {
+        return "Champ obligatoire"
+      }
+      if (control.errors?.['minLength']) {
+        return "Format invalide"
+      }
+    }
+    return null
+  }
 }
