f87031e660b42a1a10f1aa98c65b94c5a9230a3e
theo <theo.constantin01@etu.umontpellier.fr>
2025-11-20 10:38:49 +0100

    implémentation des jeux

--- frontend/src/app/jeu/jeu-form/jeu-form.ts
+++ frontend/src/app/jeu/jeu-form/jeu-form.ts
@@ -1,11 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, output, inject } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { EditeurListService } from '../../editeur/editeur-service/editeur-list-service';
+
 
 @Component({
   selector: 'app-jeu-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './jeu-form.html',
   styleUrl: './jeu-form.css',
 })
 export class JeuForm {
+private readonly editeurService = inject(EditeurListService);
 
+  readonly form = new FormGroup({  
+    nom: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(3)]
+    }),
+    
+    ageMin: new FormControl<number | undefined>(undefined, {
+      validators: [Validators.min(0)]
+    }),
+    
+    ageMax: new FormControl<number | undefined>(undefined, {
+      validators: [Validators.min(0)]
+    }),
+    
+    editeur: new FormControl<number | undefined>(undefined, {
+      validators: [Validators.required]
+    }),
+    
+    auteur: new FormControl<number | undefined>(undefined),
+    
+    type: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+    
+    taille: new FormControl<'petit' | 'grand' | undefined>(undefined)
+  });
+
+  add = output<any>()
+  submitted = false
+
+  onSubmit(): void {
+    this.submitted= true
+    if (this.form.valid) {
+      this.add.emit(this.form.value)
+      this.form.reset()
+      this.submitted = false
+    }
+  }
+
+  getErrorMessage(control: AbstractControl | null): string | null {
+    if (control != null) {  
+      if (control.errors?.['required']) {
+        return "Champ obligatoire"
+      }
+      if (control.errors?.['minlength']) {
+        const required = control.errors['minlength'].requiredLength;
+        return `Minimum ${required} caractères`;
+      }
+      if (control.errors?.['min']) {
+        return "La valeur doit être positive"
+      }
+    }
+    return null
+  }
+
+  get editeurs() {
+    return this.editeurService.editeurs();
+  }
 }
