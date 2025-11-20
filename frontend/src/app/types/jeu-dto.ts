4374e19e1f8bbf998219ddbd698819d9b4d7c990
theo <theo.constantin01@etu.umontpellier.fr>
2025-11-20 10:39:32 +0100

    ajout dto jeu

---
+++ frontend/src/app/types/jeu-dto.ts
@@ -0,0 +1,16 @@
+import { EditeurDto } from "./editeur-dto"
+import { PersonneDto } from "./personne-dto"
+
+export interface JeuDto {
+    id : number | undefined
+    nom : string 
+    
+    ageMin : number | undefined
+    ageMax : number | undefined
+    
+    editeur : EditeurDto
+    auteur : PersonneDto | undefined
+
+    type : string
+    taille : 'petit' | 'grand' | undefined
+}
